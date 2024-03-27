06890d2ccd924cc5aac9fbe9a6fb7996d9453204
JoonYoung Yoon <ZE-R0-1@github.com>
2024-03-27 10:32:19 +0900

    add MainTabType

---
+++ zRingSize/View/MainTab/MainTabType.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabType.swift
+//  zRingSize
+//
+//  Created by KMUSER on 2024/03/27.
+//
+
+import Foundation
+
+enum MainTabType: String, CaseIterable {
+    case ring
+    case finger
+    case history
+    case settings
+    
+    var title: String {
+        switch self {
+        case .ring:
+            return "반지사이즈"
+        case .finger:
+            return "손가락사이즈"
+        case .history:
+            return "기록"
+        case .settings:
+            return "설정"
+        }
+    }
+}
