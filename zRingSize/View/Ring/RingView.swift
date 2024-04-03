41152019dde1a4b5e2259ab92e30b0b6bd9ca793
JoonYoung Yoon <ZE-R0-1@github.com>
2024-04-03 14:47:20 +0900

    add MesurementModel

--- zRingSize/View/Ring/RingView.swift
+++ zRingSize/View/Ring/RingView.swift
@@ -13,14 +13,7 @@ struct RingView: View {
     
     @State private var showingAlert = false
     @State private var title = ""
-    
-    private let measurementFormatter: MeasurementFormatter = {
-        let formatter = MeasurementFormatter()
-        formatter.unitOptions = .providedUnit
-        formatter.numberFormatter.maximumFractionDigits = 2 // 소수점 자리수 설정
-        formatter.numberFormatter.usesGroupingSeparator = true // 천 단위로 ,(쉼표) 표시
-        return formatter
-    }()
+    @State private var selectedRingSizeIndex = 0
     
     var body: some View {
         NavigationView {
@@ -30,68 +23,41 @@ struct RingView: View {
                     Circle()
                         .stroke(style: StrokeStyle(lineWidth: 2, dash: [5]))
                         .foregroundColor(Color.black)
-                        .frame(width: viewModel.ringSize, height: viewModel.ringSize)
-                    Text("반지")
+                        .frame(width: viewModel.ringDiameter, height: viewModel.ringDiameter)
+                    Text(viewModel.formattedRingSize)
                 }
                 .padding()
                 
+                
+
+                
                 Spacer()
-                
-                // 반지 지름 표시
-                Text("반지 지름 : \(formattedDiameter())")
-                    .padding(.horizontal, 20)
-                
-                // 반지 둘레 표시
-                Text("반지 둘레 : \(formattedCircumference())")
-                    .padding(.horizontal, 20)
-                
-                HStack {
-                    Button("-") {
-                        viewModel.ringSize -= 50 // 슬라이더 값 감소
-                    }
-                    Slider(value: $viewModel.ringSize, in: 100...250)
-                        .padding(.horizontal, 10)
-                    Button("+") {
-                        viewModel.ringSize += 50 // 슬라이더 값 증가
-                    }
-                }
-                .padding([.leading, .trailing], 70)
+                Text("반지 지름 : \(viewModel.ringDiameter)")
+                Text("반지 둘레 : \(viewModel.calculateCircumference())")
             }
-            .navigationBarTitle("RingSizer", displayMode: .inline)
-            .navigationBarItems(
-                trailing: Button("저장") {
-                    showingAlert = true
-                }
-            )
-            .alert("링사이즈", isPresented: $showingAlert) {
-                TextField("제목을 적어주세요", text: $title)
-                Button("확인", action: saveData)
-                Button("취소") {
-                    showingAlert = false
-                }
+            .padding([.leading, .trailing], 70)
+        }
+        .navigationBarTitle("RingSizer", displayMode: .inline)
+        .navigationBarItems(
+            trailing: Button("저장") {
+                showingAlert = true
+            }
+        )
+        .alert("링사이즈", isPresented: $showingAlert) {
+            TextField("제목을 적어주세요", text: $title)
+            Button("확인", action: saveData)
+            Button("취소") {
+                showingAlert = false
             }
         }
     }
     
-    // 지름을 형식화하여 반환
-    private func formattedDiameter() -> String {
-        let diameter = Measurement(value: viewModel.ringSize, unit: UnitLength.millimeters)
-        return measurementFormatter.string(from: diameter)
-    }
-    
-    // 반지 둘레를 형식화하여 반환
-    private func formattedCircumference() -> String {
-        let radius = viewModel.ringSize / 2
-        let circumferenceValue = 2 * Double.pi * radius
-        let circumference = Measurement(value: circumferenceValue, unit: UnitLength.millimeters)
-        return measurementFormatter.string(from: circumference)
-    }
     
     func saveData() {
         let newItem = Sizer(context: viewContext)
         newItem.title = title
         newItem.date = Date()
-        newItem.size = Double(viewModel.ringSize)
+        newItem.size = Double(viewModel.ringDiameter)
         
         title = ""
         
--- zRingSize/View/Ring/RingViewModel.swift
+++ zRingSize/View/Ring/RingViewModel.swift
@@ -8,5 +8,49 @@
 import SwiftUI
 
 class RingViewModel: ObservableObject {
-    @Published var ringSize: CGFloat = 100
+    @Published var ringDiameter: Double = 1.50
+    
+    let ringSizes: [String: CGFloat] = [
+        "7호": 1.50,
+        "8호": 1.54,
+        "9호": 1.58,
+        "10호": 1.60,
+        "11호": 1.64,
+        "12호": 1.66,
+        "13호": 1.70,
+        "14호": 1.72,
+        "15호": 1.77,
+        "16호": 1.80,
+        "17호": 1.83,
+        "18호": 1.86,
+        "19호": 1.90,
+        "20호": 1.93,
+        "21호": 1.96,
+        "22호": 1.98,
+        "23호": 2.01,
+        "24호": 2.05,
+        "25호": 2.08,
+        "26호": 2.11,
+        "27호": 2.15,
+        "28호": 2.18,
+        "29호": 2.21,
+        "30호": 2.25
+    ]
+    
+    var formattedRingSize: String {
+        return Array(ringSizes.keys)[Int(ringDiameter * 2 - 3)] // assuming ringDiameter is in range of ringSizes
+    }
+    
+    func updateRingDiameter(index: Int) {
+        guard index >= 0 && index < ringSizes.count else { return }
+        let ringSizeValues = Array(ringSizes.values)
+        ringDiameter = ringSizeValues[index]
+    }
+    
+    func calculateCircumference() -> CGFloat {
+        let diameter = ringDiameter
+        let circumference = diameter * CGFloat.pi
+        return circumference
+    }
 }
+
