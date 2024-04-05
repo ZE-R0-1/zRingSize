1db3d71d56806d8dbf43119f61c27a210a19bf4e
JoonYoung Yoon <ZE-R0-1@github.com>
2024-04-05 16:21:58 +0900

    SettingsView Error 수정

--- zRingSize/View/Settings/SizeChartView.swift
+++ zRingSize/View/Settings/SizeChartView.swift
@@ -16,37 +16,39 @@ struct SizeChartView: View {
     ]
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(measurementModel.ringSizes.sorted(by: { Int($0.key.replacingOccurrences(of: "호", with: "")) ?? 0 < Int($1.key.replacingOccurrences(of: "호", with: "")) ?? 0 }), id: \.key) { key, value in
-                        Text("\(key)")
-                            .padding()
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .background(Color.white)
-                            .cornerRadius(8)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 8)
-                                    .stroke(Color.gray, lineWidth: 1)
-                            )
-                        
-                        Text(String(format: "%.1f mm", value * 10))
-                            .padding()
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .background(Color.white)
-                            .cornerRadius(8)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 8)
-                                    .stroke(Color.gray, lineWidth: 1)
-                            )
-                    }
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(measurementModel.ringSizes.sorted(by: { Int($0.key.replacingOccurrences(of: "호", with: "")) ?? 0 < Int($1.key.replacingOccurrences(of: "호", with: "")) ?? 0 }), id: \.key) { key, value in
+                    Text("\(key)")
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .background(Color(UIColor.systemBackground))
+                        .foregroundColor(Color(UIColor.label))
+                        .cornerRadius(8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                    
+                    Text(String(format: "%.1f mm", value * 10))
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .background(Color(UIColor.systemBackground))
+                        .foregroundColor(Color(UIColor.label))
+                        .cornerRadius(8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
                 }
-                .padding()
             }
-            .navigationBarTitle("반지 사이즈")
+            .padding()
         }
+        .background(Color(UIColor.systemBackground))
+        .navigationBarTitle("반지 사이즈")
     }
 }
+
 
 struct SizeChartView_Previews: PreviewProvider {
     static var previews: some View {
